a301a445d7f8034c9dd751ba20b370d4ad0043d9
Jeremy <pawjwp@users.noreply.github.com>
2026-07-14 00:19:08 -0400

    Fixed mouse getting reset when switching MAT tabs

--- common/src/main/java/hardcorequesting/common/client/interfaces/mat/MatScreens.java
+++ common/src/main/java/hardcorequesting/common/client/interfaces/mat/MatScreens.java
@@ -4,6 +4,8 @@ import hardcorequesting.common.items.mat.MatMode;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.protocol.game.ServerboundContainerClosePacket;
 import net.minecraft.world.entity.player.Player;
 
 // Clientside MAT opening for certain modes
@@ -11,10 +13,23 @@ import net.minecraft.world.entity.player.Player;
 public class MatScreens {
     public static void open(Player player, MatMode mode) {
         if (player == null) return;
+        closeOpenContainer();
         if (mode == MatMode.QUEST) {
             GuiMatQuestBook.displayGui(player);
         } else {
             Minecraft.getInstance().setScreen(new GuiMatPlaceholder(player, mode));
+        }
+    }
+
+    // Used to properly close server containers when switching to another tab
+    private static void closeOpenContainer() {
+        Minecraft mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        if (player != null && player.containerMenu != player.inventoryMenu) {
+            if (mc.getConnection() != null) {
+                mc.getConnection().send(new ServerboundContainerClosePacket(player.containerMenu.containerId));
+            }
+            player.containerMenu = player.inventoryMenu;
         }
     }
 }

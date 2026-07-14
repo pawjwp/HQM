624e850d641cf328d9b834ff4537a93f9763218a
Jeremy <pawjwp@users.noreply.github.com>
2026-07-14 00:19:19 -0400

    MAT keybinds

--- common/src/main/java/hardcorequesting/common/client/QuestBookKeyHandler.java
+++ common/src/main/java/hardcorequesting/common/client/QuestBookKeyHandler.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.platform.InputConstants;
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import hardcorequesting.common.client.interfaces.GuiQuestBook;
 import hardcorequesting.common.config.HQMConfig;
+import hardcorequesting.common.items.mat.MatHandler;
+import hardcorequesting.common.items.mat.MatMode;
 import hardcorequesting.common.network.GeneralUsage;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -15,10 +17,23 @@ import net.minecraft.nbt.CompoundTag;
 public class QuestBookKeyHandler {
     public static final String CATEGORY = "key.hqm.category";
     public static KeyMapping openBook;
+    public static KeyMapping openMatDefault;
+    public static KeyMapping openMatCrafting;
+    public static KeyMapping openMatTracking;
+    public static KeyMapping openMatStorage;
 
     public static void register() {
-        openBook = new KeyMapping("key.hqm.openBook", InputConstants.UNKNOWN.getValue(), CATEGORY);
-        KeyMappingRegistry.register(openBook);
+        openBook = register("key.hqm.openBook");
+        openMatDefault = register("key.hqm.mat.default");
+        openMatCrafting = register("key.hqm.mat.crafting");
+        openMatTracking = register("key.hqm.mat.tracking");
+        openMatStorage = register("key.hqm.mat.storage");
+    }
+
+    private static KeyMapping register(String name) {
+        KeyMapping mapping = new KeyMapping(name, InputConstants.UNKNOWN.getValue(), CATEGORY);
+        KeyMappingRegistry.register(mapping);
+        return mapping;
     }
 
     public static void handleTick(Minecraft mc) {
@@ -26,8 +41,29 @@ public class QuestBookKeyHandler {
             if (mc.screen instanceof GuiQuestBook) {
                 if (HQMConfig.getInstance().Keybind.TOGGLE) mc.setScreen(null);
             } else if (mc.screen == null && mc.player != null) {
-                GeneralUsage.REQUEST_BOOK_OPEN.sendMessageToServer(new CompoundTag());
+                if (matEnabled() && MatMode.QUEST.isEnabled() && MatHandler.hasMat(mc.player)) {
+                    GeneralUsage.sendOpenMatMode(MatMode.QUEST, false);
+                } else {
+                    GeneralUsage.REQUEST_BOOK_OPEN.sendMessageToServer(new CompoundTag());
+                }
             }
         }
+
+        handleModeKey(mc, openMatDefault, MatMode.DEFAULT);
+        handleModeKey(mc, openMatCrafting, MatMode.CRAFTING);
+        handleModeKey(mc, openMatTracking, MatMode.TRACKING);
+        handleModeKey(mc, openMatStorage, MatMode.STORAGE);
+    }
+
+    private static void handleModeKey(Minecraft mc, KeyMapping mapping, MatMode mode) {
+        while (mapping.consumeClick()) {
+            if (mc.screen == null && mc.player != null && matEnabled() && mode.isEnabled() && MatHandler.hasMat(mc.player)) {
+                GeneralUsage.sendOpenMatMode(mode, false);
+            }
+        }
+    }
+
+    private static boolean matEnabled() {
+        return HQMConfig.getInstance().MAT.ENABLE_MAT;
     }
 }

7401924ceb9636c0e233facdc7a4b8b909b3a8f2
Jeremy <pawjwp@users.noreply.github.com>
2026-09-25 08:18:44 -0400

    Keep MAT Crafting table open until manually closed
    Keep the MAT crafting table open until it's manually closed by the player, since it can be opened anywhere and does not need the player to remain in one spot
    Removed manual item return when inventory is closed

--- common/src/main/java/hardcorequesting/common/inventory/MatCraftingMenu.java
+++ common/src/main/java/hardcorequesting/common/inventory/MatCraftingMenu.java
@@ -5,8 +5,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.CraftingMenu;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
 
 /**
  * A retextured copy of the vanilla CraftingMenu with a tab bar
@@ -25,21 +23,9 @@ public class MatCraftingMenu extends CraftingMenu {
         return ModMenus.matCrafting.get();
     }
 
+    // Stay open until the player manually closes the UI
     @Override
-    public void removed(Player player) {
-        super.removed(player);
-        // Give items back to the player when the inventory is closed, might
-        if (!player.level().isClientSide) {
-            for (int i = 1; i <= 9; i++) {
-                Slot slot = this.slots.get(i);
-                ItemStack stack = slot.getItem();
-                if (!stack.isEmpty()) {
-                    slot.set(ItemStack.EMPTY);
-                    if (!player.getInventory().add(stack)) {
-                        player.drop(stack, false);
-                    }
-                }
-            }
-        }
+    public boolean stillValid(Player player) {
+        return true;
     }
 }
